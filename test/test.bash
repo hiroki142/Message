6e77fe0ae4a94079295ee595c8d88ec60b1a5e01
Hiroki Nukui <s21C1092OD@s.chibakoudai.jp>
2023-01-07 20:06:43 +0900

    add a test

--- test/test.bash
+++ test/test.bash
@@ -7,7 +7,7 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-ros2 topic pub --once /input log_msgs/msg/Log "{log: Hello World}" &
+ros2 topic pub --once /input log_msgs/msg/Log '{log: Hello World}' &
 timeout 1 ros2 launch Message talk_listen.launch.py > /tmp/Message.log
 
 cat /tmp/Message.log |
